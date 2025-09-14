7cc9696f18e85e3a08e2fb4a953d32d376d3ca0c
Yun <mosy2266@naver.com>
2025-09-14 19:11:07 +0900

    Week 1 과제 제출

---
+++ week1/src/Books.java
@@ -0,0 +1,34 @@
+public class Books {
+    String title;
+    String author;
+    String genre;
+    String publisher;
+    int pages;
+
+    public Books(String title, String author, String genre, String publisher, int pages) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.publisher = publisher;
+        this.pages = pages;
+    }
+}
+
+class EBooks extends Books {
+    String platform;
+    Boolean isAvailable;
+
+    public EBooks(String title, String author, String genre, String publisher, int pages,
+                  String platform, Boolean isAvailable) {
+        super(title, author, genre, publisher, pages);
+        this.platform = platform;
+        this.isAvailable = isAvailable;
+    }
+}
+
+class RealBook extends EBooks {
+    public RealBook(String title, String author, String genre, String publisher, int pages,
+                    String platform, Boolean isAvailable) {
+        super(title, author, genre, publisher, pages, platform, isAvailable);
+    }
+}
